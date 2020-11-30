ead2af583c3f22ada1ab4c5182ff89fcf2d5dda1
Braispg <braispga@gmail.com>
2020-11-30 17:31:06 +0100

    formularioReserva-noFucional

--- src/app/menus/menus.component.ts
+++ src/app/menus/menus.component.ts
@@ -14,7 +14,9 @@ export class MenusComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  menus = [{nombre: 'menu 1', primerPlato: 'puré', segundoPlato: 'Costillas', precio: 15}, 
-  {nombre: 'menu 2', primerPlato: 'Sopa de fideos', segundoPlato: 'Bacalao', precio: 13}];
+  menus = [  {nombre: 'Menú del día', primerPlato: 'Ensalada con queso de cabra', segundoPlato: 'Bacalao a la brasa', postre: 'Flan de huevo', precio: 15},
+  {nombre: 'menu 1', primerPlato: 'puré', segundoPlato: 'Costillas', postre: 'Arroz con leche', precio: 15}, 
+  {nombre: 'menu 2', primerPlato: 'Sopa de fideos', segundoPlato: 'Bacalao', postre: 'Crema catalana', precio: 13},
+  {nombre: 'Menu infantil', primerPlato: 'Crema de calabaza', segundoPlato:'Hamburguesa de pollo con queso', postre: 'Natillas', precio: 10}];
 
 }
